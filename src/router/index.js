11c949768255096134a5661687fa541ab2bb810b
JabzV <vestidas.jabezjoshua@gmail.com>
2025-10-01 20:34:47 +0800

    ver(1.0.4): fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Dashboard from '../components/pages/Dashboard.vue'
 import Users from '../components/pages/Users.vue'
@@ -8,13 +8,12 @@ import History from '../components/pages/History.vue'
 export const routes = [
     { path: '/', name: 'Dashboard', component: Dashboard, icon: 'pi pi-home' },
     { path: '/users', name: 'Users', component: Users, icon: 'pi pi-users' },
-    
     { path: '/history', name: 'History', component: History, icon: 'pi pi-history' },
     { path: '/settings', name: 'Settings', component: Settings, icon: 'pi pi-cog' },
   ]
 
   const router = createRouter({
-    history: createWebHistory(), // use history mode
+    history: createWebHashHistory(), // Use hash mode for Electron (works with file:// protocol)
     routes,
   })
   
